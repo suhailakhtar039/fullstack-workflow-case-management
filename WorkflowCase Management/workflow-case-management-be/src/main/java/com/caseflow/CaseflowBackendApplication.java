6f6e6cc9ddcae48631e107d43cb1f1e671bfff70
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-18 15:38:49 +0530

    backend in spring boot created

---
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/CaseflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.caseflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseflowBackendApplication.class, args);
+	}
+
+}
---
+++ WorkflowCase Management/workflow-case-management-be/src/test/java/com/caseflow/CaseflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caseflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
