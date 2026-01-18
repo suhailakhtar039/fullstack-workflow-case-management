70a00aa22b73dc5213f8b5d47d72baa594e71d85
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-18 16:08:09 +0530

    made package structure and create class base entity

---
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/domain/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.caseflow.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+
+}
