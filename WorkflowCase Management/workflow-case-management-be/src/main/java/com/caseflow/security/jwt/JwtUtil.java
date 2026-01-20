06591bd8565835180bc17af5f05ec78ce08cb461
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-20 20:31:05 +0530

    jwt token added

---
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/security/jwt/JwtUtil.java
@@ -0,0 +1,51 @@
+package com.caseflow.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private static final long EXPIRATION_MS = 60 * 60 * 1000; // 1 hour
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    // generating token
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
+                .signWith(key)
+                .compact();
+    }
+
+    // get username
+    public String extractUsername(String token) {
+        return parseClaims(token).getSubject();
+    }
+
+    // token valid or not
+    public boolean isTokenValid(String token) {
+        try {
+            parseClaims(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    // Claims
+    private Claims parseClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
