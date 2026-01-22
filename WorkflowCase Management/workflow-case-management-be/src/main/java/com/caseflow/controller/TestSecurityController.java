a16dfb7e657cf9a6f597b4804be700b013c6a379
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-22 22:32:48 +0530

    constants added

---
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/controller/TestSecurityController.java
@@ -0,0 +1,4 @@
+package com.caseflow.controller;
+
+public class TestSecurityController {
+}
---
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/util/SecurityRoles.java
@@ -0,0 +1,11 @@
+package com.caseflow.util;
+
+public final class SecurityRoles {
+    private SecurityRoles(){}
+
+    public static final String ADMIN = "ADMIN";
+    public static final String CASE_MANAGER = "CASE_MANAGER";
+    public static final String REVIEWER = "REVIEWER";
+    public static final String APPROVER = "APPROVER";
+    public static final String VIEWER = "VIEWER";
+}
