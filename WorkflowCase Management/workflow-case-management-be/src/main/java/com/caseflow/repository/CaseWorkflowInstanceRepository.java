719c8cae2117b1c81cbfe6b4af61e804f8f762e4
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-27 19:56:17 +0530

    custom query added

--- WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/repository/CaseWorkflowInstanceRepository.java
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/repository/CaseWorkflowInstanceRepository.java
@@ -2,11 +2,19 @@ package com.caseflow.repository;
 
 import com.caseflow.domain.CaseWorkflowInstance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CaseWorkflowInstanceRepository extends JpaRepository<CaseWorkflowInstance, Long> {
     Optional<CaseWorkflowInstance> findByCaseEntityId(Long caseId);
+
+    @Query("SELECT i FROM CaseWorkflowInstance i" +
+            " JOIN WorkflowStep s " +
+            "ON s.workflow = i.workflow AND s.stepOrder = i.currentStepOrder" +
+            " WHERE s.allowedRole=:role")
+    List<CaseWorkflowInstance> findPendingByRole(String role);
 }
--- WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/repository/WorkflowStepRepository.java
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/repository/WorkflowStepRepository.java
@@ -3,6 +3,7 @@ package com.caseflow.repository;
 import com.caseflow.domain.Workflow;
 import com.caseflow.domain.WorkflowStep;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +11,7 @@ import java.util.Optional;
 @Repository
 public interface WorkflowStepRepository extends JpaRepository<WorkflowStep, Long> {
     Optional<WorkflowStep> findByWorkflowAndStepOrder(Workflow workflow, int stepOrder);
+
+    @Query("SELECT w FROM WorkflowStep w WHERE w.workflow= :workflow AND w.stepOrder = :stepOrder")
+    Optional<WorkflowStep> findStep(Workflow workflow, int stepOrder);
 }
