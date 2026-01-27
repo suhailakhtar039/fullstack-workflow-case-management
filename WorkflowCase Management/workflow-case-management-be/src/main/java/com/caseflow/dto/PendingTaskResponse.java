a35190c60346158e81162432d284bfa59426149b
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-27 20:03:59 +0530

    PendingTaskResponse Dto created

---
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/dto/PendingTaskResponse.java
@@ -0,0 +1,11 @@
+package com.caseflow.dto;
+
+public record PendingTaskResponse(
+        Long caseId,
+        String caseNumber,
+        String title,
+        String currentStatus,
+        String pendingRole,
+        int stepOrder
+) {
+}
