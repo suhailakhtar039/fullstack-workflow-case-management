995a8ec9041772c8d37b12cb5e9cafb2d882616a
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-31 15:28:08 +0530

    add validation using NotNull and NotBlank in CaseCreateRequest

--- WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/dto/CaseCreateRequest.java
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/dto/CaseCreateRequest.java
@@ -2,11 +2,20 @@ package com.caseflow.dto;
 
 
 import com.caseflow.domain.enums.Priority;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CaseCreateRequest(
+        @NotBlank(message = "Title is required")
         String title,
+
+        @NotBlank(message = "Description is required")
         String description,
+
+        @NotBlank(message = "Type is required")
         String type,
+
+        @NotNull(message = "Priority is required")
         Priority priority
 ) {
 }
