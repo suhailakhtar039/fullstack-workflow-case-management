8ba06cfabd4f3200813d3079e22dd64ff6a296b7
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-31 12:22:50 +0530

    add page and pageable in CaseRepository

--- WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/repository/CaseRepository.java
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/repository/CaseRepository.java
@@ -1,6 +1,9 @@
 package com.caseflow.repository;
 
 import com.caseflow.domain.Case;
+import com.caseflow.domain.enums.CaseStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,10 @@ import java.util.Optional;
 @Repository
 public interface CaseRepository extends JpaRepository<Case, Long> {
     Optional<Case> findByCaseNumber(String caseNumber);
+
+    Page<Case> findByStatus(CaseStatus status, Pageable pageable);
+
+    Page<Case> findByCaseNumberContainingIgnoreCase(String caseNumber, Pageable pageable);
+
+    Page<Case> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 }
