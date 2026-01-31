3087456117d92ce21ebd9a345a3e48565b5a808e
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-31 23:17:27 +0530

    made auth interceptor to check for JWT

--- WorkflowCase Management/workflow-case-management-fe/src/app/core/interceptors/auth-interceptor.ts
+++ WorkflowCase Management/workflow-case-management-fe/src/app/core/interceptors/auth-interceptor.ts
@@ -1,11 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('token');
+  const authService = inject(AuthService);
+  const token = authService.getToken();
   if (token) {
     req = req.clone({
       setHeaders: {
-        Authorization: `Bearer: ${token}`,
+        Authorization: `Bearer ${token}`,
       },
     });
   }
