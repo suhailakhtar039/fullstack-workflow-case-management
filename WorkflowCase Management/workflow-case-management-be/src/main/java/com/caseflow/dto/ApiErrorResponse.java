cfd02a3e0435acded54d6d929a96324250d1bc18
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-31 15:26:45 +0530

    add validation in ApiErrorResponse

--- WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/dto/ApiErrorResponse.java
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/dto/ApiErrorResponse.java
@@ -1,11 +1,18 @@
 package com.caseflow.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ApiErrorResponse(
+        @NotBlank(message = "Always enter status")
         int status,
+        @NotBlank(message = "Tell the error")
         String error,
+        @NotBlank(message = "Get message from unique exception handler")
         String message,
+        @NotBlank(message = "Give the path where we are getting error")
         String path,
+        @NotBlank(message = "Give the timestamp")
         long timestamp
 ) {
 }
