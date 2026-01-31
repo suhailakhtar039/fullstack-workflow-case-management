da996a8aafec7a72db7e855eadf4a54a5ea58fa7
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-31 23:15:29 +0530

    modified CorsConfig and SecurityConfig

--- WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/config/SecurityConfig.java
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.caseflow.security.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -37,6 +38,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
