4e340a5c2533e798641c0c5da9d3def829dd93b9
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-31 23:16:30 +0530

    made authService

---
+++ WorkflowCase Management/workflow-case-management-fe/src/app/core/auth/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+interface LoginResponse {
+  token: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly TOKEN_KEY = 'caseflow_token';
+  private readonly API_URL = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<LoginResponse> {
+    return this.http
+      .post<LoginResponse>(`${this.API_URL}/api/auth/login`, { username, password })
+      .pipe(tap((res) => this.saveToken(res.token)));
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.TOKEN_KEY);
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+}
