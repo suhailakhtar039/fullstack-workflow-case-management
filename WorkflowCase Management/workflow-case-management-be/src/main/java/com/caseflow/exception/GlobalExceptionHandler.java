02142f39fd34e3fe99cf705c8bc306cc6a4c5c86
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-31 15:43:04 +0530

    duplicate entry added of Item Not Found Exception

--- WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/exception/GlobalExceptionHandler.java
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/exception/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
