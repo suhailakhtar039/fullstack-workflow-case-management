7d01c5f6103ba6aadbbb9a8b80dc8be5464f4553
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-01-31 23:16:02 +0530

    used app-module and app-routing-module.ts

--- WorkflowCase Management/workflow-case-management-fe/src/app/app-module.ts
+++ WorkflowCase Management/workflow-case-management-fe/src/app/app-module.ts
@@ -4,19 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { LogInComponent } from './core/auth/log-in-component/log-in-component';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth-interceptor';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MatIconModule
-  ],
+  declarations: [App, LogInComponent],
+  imports: [BrowserModule, AppRoutingModule, MatIconModule, FormsModule],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient(withInterceptors([authInterceptor])),
   ],
-  bootstrap: [App]
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
--- WorkflowCase Management/workflow-case-management-fe/src/app/app-routing-module.ts
+++ WorkflowCase Management/workflow-case-management-fe/src/app/app-routing-module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './core/auth/log-in-component/log-in-component';
+import { authGuardGuard } from './core/guard/auth-guard-guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LogInComponent },
+  {
+    path: 'dashboard',
+    canActivate: [authGuardGuard],
+    loadChildren: () =>
+      import('./features/dashboard/dashboard-module').then((m) => m.DashboardModule),
+  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
