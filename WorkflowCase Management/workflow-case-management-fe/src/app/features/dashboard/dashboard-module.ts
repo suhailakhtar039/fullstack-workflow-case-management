39bc2bf9fc8f722fbe402c134af7913de0b4fd2c
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-02-07 16:33:46 +0530

    new angular material module added

--- WorkflowCase Management/workflow-case-management-fe/src/app/features/dashboard/dashboard-module.ts
+++ WorkflowCase Management/workflow-case-management-fe/src/app/features/dashboard/dashboard-module.ts
@@ -5,15 +5,24 @@ import { DashboardRoutingModule } from './dashboard-routing-module';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { CaseList } from './pages/case-list/case-list';
 
+// angular material imports
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
-  declarations: [
-    Dashboard,
-    CaseList
-  ],
+  declarations: [Dashboard, CaseList],
   imports: [
     CommonModule,
-    DashboardRoutingModule
-  ]
+    DashboardRoutingModule,
+    // angular material imports
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+  ],
 })
-export class DashboardModule { }
+export class DashboardModule {}
