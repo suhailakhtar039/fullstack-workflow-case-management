a9ba62cfcd2cd87640fea80bff1cdc1b0cff555e
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-02-07 17:24:58 +0530

    modify case list and use paging technique

--- WorkflowCase Management/workflow-case-management-fe/src/app/features/dashboard/pages/case-list/case-list.ts
+++ WorkflowCase Management/workflow-case-management-fe/src/app/features/dashboard/pages/case-list/case-list.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { CaseService } from '../../services/case.services';
 
 @Component({
   selector: 'app-case-list',
@@ -8,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class CaseList {
   displayedColumns: string[] = ['caseNumber', 'title', 'status'];
+  cases: any[] = [];
+  totalElements = 0;
+  pageSize = 10;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  constructor(
+    private caseService: CaseService,
+    private cdr: ChangeDetectorRef,
+  ) {}
+
+  ngOnInit() {
+    this.loadCases(0, this.pageSize);
+  }
+
+  loadCases(page: number, size: number) {
+    this.caseService.getCases(page, size).subscribe((response) => {
+      console.log('response = ' + response);
+      console.log('response content = ' + response.content);
+      console.log('total elements: ' + response.totalElements);
+      this.cases = response.content;
+      this.totalElements = response.totalElements;
+      this.cdr.detectChanges();
+    });
+  }
+
+  onPageChange(event: any) {
+    this.loadCases(event.pageIndex, event.pageSize);
+  }
 }
