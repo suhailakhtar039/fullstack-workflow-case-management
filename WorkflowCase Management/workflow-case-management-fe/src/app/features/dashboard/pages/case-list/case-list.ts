fe398df7de8b1a50c4d6b742a845d8c72b0ceec9
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-02-07 23:35:59 +0530

    implementing search functionality

--- WorkflowCase Management/workflow-case-management-fe/src/app/features/dashboard/pages/case-list/case-list.ts
+++ WorkflowCase Management/workflow-case-management-fe/src/app/features/dashboard/pages/case-list/case-list.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { CaseService } from '../../services/case.services';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-case-list',
@@ -14,6 +15,12 @@ export class CaseList {
   totalElements = 0;
   pageSize = 10;
 
+  isLoading = false;
+  hasError = false;
+  errorMessage = '';
+  searchTerm = '';
+  filteredCases: any[] = [];
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   constructor(
     private caseService: CaseService,
@@ -25,17 +32,52 @@ export class CaseList {
   }
 
   loadCases(page: number, size: number) {
-    this.caseService.getCases(page, size).subscribe((response) => {
-      console.log('response = ' + response);
-      console.log('response content = ' + response.content);
-      console.log('total elements: ' + response.totalElements);
-      this.cases = response.content;
-      this.totalElements = response.totalElements;
-      this.cdr.detectChanges();
-    });
+    console.log('LOAD START');
+    this.isLoading = true;
+
+    this.caseService
+      .getCases(page, size)
+      .pipe(
+        finalize(() => {
+          console.log('FINALIZE CALLED isLoading: ', this.isLoading);
+          this.isLoading = false;
+          this.cdr.markForCheck();
+        }),
+      )
+      .subscribe({
+        next: (response: any) => {
+          console.log('NEXT CALLED isLoading: ', this.isLoading);
+          this.cases = response.content;
+          this.filteredCases = response.content;
+          this.totalElements = response.totalElements;
+        },
+        error: (error) => {
+          console.log('ERROR CALLED', error);
+          this.hasError = true;
+          this.errorMessage = 'Failed to load cases!';
+          this.cases = [];
+          this.totalElements = 0;
+        },
+      });
   }
 
   onPageChange(event: any) {
+    // console.log('check in page change');
+    this.pageSize = event.pageSize;
     this.loadCases(event.pageIndex, event.pageSize);
+  }
+
+  searchCaseAndTitle() {
+    const term = this.searchTerm.toLowerCase().trim();
+
+    this.filteredCases = this.cases.filter((c) => {
+      return c.caseNumber.toLowerCase().includes(term) || c.title.toLowerCase().includes(term);
+    });
+  }
+
+  onReset() {
+    this.searchTerm = '';
+    this.paginator.firstPage();
+    this.loadCases(0, this.pageSize);
   }
 }
