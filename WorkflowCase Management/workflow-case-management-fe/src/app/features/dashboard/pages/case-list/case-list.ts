0c27427a4fb4bd9fa84c3b97f53b61af389607e0
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-02-07 13:57:04 +0530

    case list component added

---
+++ WorkflowCase Management/workflow-case-management-fe/src/app/features/dashboard/pages/case-list/case-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-case-list',
+  standalone: false,
+  templateUrl: './case-list.html',
+  styleUrl: './case-list.css',
+})
+export class CaseList {
+
+}
