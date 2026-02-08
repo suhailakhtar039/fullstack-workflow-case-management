af2f09c61de3c6bdb5d71d72249d12291f73db5e
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-02-08 13:55:57 +0530

    new queries created

--- WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/repository/CaseRepository.java
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/repository/CaseRepository.java
@@ -5,6 +5,8 @@ import com.caseflow.domain.enums.CaseStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -22,4 +24,27 @@ public interface CaseRepository extends JpaRepository<Case, Long> {
     Page<Case> findByCreatedByUsername(String username, Pageable pageable);
 
     long countByStatus(CaseStatus status);
+
+    @Query("SELECT c FROM Case c " +
+            "WHERE (:caseNumber IS NOT NULL AND LOWER(c.caseNumber)" +
+            " LIKE LOWER(CONCAT('%', :caseNumber, '%')))" +
+            " OR (:title IS NOT NULL AND LOWER(c.title)" +
+            " LIKE LOWER(CONCAT('%', :title, '%')))")
+    Page<Case> searchByCaseNumberOrTitle(
+            @Param("caseNumber") String caseNumber,
+            @Param("title") String title,
+            Pageable pageable
+    );
+
+    @Query("SELECT c FROM Case c" +
+            " WHERE ( (:caseNumber IS NOT NULL AND LOWER(c.caseNumber) LIKE LOWER(CONCAT('%', :caseNumber, '%')))" +
+            " OR (:title IS NOT NULL AND LOWER(c.title)" +
+            " LIKE LOWER(CONCAT('%', :title, '%')))) AND c.status = :status")
+    Page<Case> searchByCaseNumberOrTitleAndStatus(
+            @Param("caseNumber") String caseNumber,
+            @Param("title") String title,
+            @Param("status") CaseStatus status,
+            Pageable pageable
+    );
+
 }
