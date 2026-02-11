2b6a3da8c43d9cd001dca03b0f7d172f815ae22a
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-02-11 22:28:38 +0530

    change api using environment file

--- WorkflowCase Management/workflow-case-management-fe/src/app/features/dashboard/services/case.services.ts
+++ WorkflowCase Management/workflow-case-management-fe/src/app/features/dashboard/services/case.services.ts
@@ -1,12 +1,16 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CaseService {
+  baseApiUrl = environment.apiBaseUrl;
+
   constructor(private http: HttpClient) {}
+
   getCases(page: number, size: number, search?: string, status?: string, myCases?: boolean) {
     let params: any = { page, size };
 
@@ -19,10 +23,14 @@ export class CaseService {
       params.status = status;
     }
 
-    const url = myCases
-      ? 'http://localhost:8080/api/cases/my'
-      : 'http://localhost:8080/api/cases/search';
+    const url = myCases ? this.baseApiUrl + '/cases/my' : this.baseApiUrl + '/cases/search';
 
     return this.http.get<any>(url, { params });
+  }
+
+  transitionCase(caseId: number, status: string) {
+    return this.http.patch<void>(environment.apiBaseUrl + '/cases/' + caseId + 'status', null, {
+      params: { status },
+    });
   }
 }
