5e9e87cc89d25821aff4450b2dfdc219f36d9f9c
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-02-11 22:44:54 +0530

    different angular material fields added

--- WorkflowCase Management/workflow-case-management-fe/src/app/app-module.ts
+++ WorkflowCase Management/workflow-case-management-fe/src/app/app-module.ts
@@ -3,15 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { LogInComponent } from './core/auth/log-in-component/log-in-component';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
 
+// import only angular material module
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 @NgModule({
   declarations: [App, LogInComponent],
-  imports: [BrowserModule, AppRoutingModule, MatIconModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    // angular material module
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+  ],
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(withInterceptors([authInterceptor])),
