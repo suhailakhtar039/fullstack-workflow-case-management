b1489195ce69e97d6bf78c098aee41b133e07571
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-02-12 22:25:45 +0530

    role guard created

---
+++ WorkflowCase Management/workflow-case-management-fe/src/app/core/guard/role-guard.ts
@@ -0,0 +1,23 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+
+export const roleGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const expectedRoles = route.data?.['roles'] as string[];
+
+  if (!expectedRoles || expectedRoles.length === 0) return true;
+
+  const userRoles = authService.getRoles();
+
+  const hasAccess = expectedRoles.some((role) => userRoles.includes(role));
+
+  if (!hasAccess) {
+    router.navigate(['/dashboard']);
+    return false;
+  }
+
+  return true;
+};
