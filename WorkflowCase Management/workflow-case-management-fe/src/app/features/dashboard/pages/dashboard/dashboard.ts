8e60e58efae43c2e43a0cc6588bfc72e57dc6bc1
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-02-14 13:17:13 +0530

    add changes for sidebar

--- WorkflowCase Management/workflow-case-management-fe/src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ WorkflowCase Management/workflow-case-management-fe/src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../core/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+  ) {}
 
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login'], { replaceUrl: true });
+  }
 }
