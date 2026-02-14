90f6be0b868ef67104f407eb100f376d20a721f9
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-02-14 13:54:26 +0530

    add guard for forbidden

--- WorkflowCase Management/workflow-case-management-fe/src/app/app-routing-module.ts
+++ WorkflowCase Management/workflow-case-management-fe/src/app/app-routing-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './core/auth/log-in-component/log-in-component';
 import { authGuardGuard } from './core/guard/auth-guard-guard';
+import { Forbidden } from './features/dashboard/pages/forbidden/forbidden';
 
 const routes: Routes = [
   { path: 'login', component: LogInComponent },
@@ -10,6 +11,11 @@ const routes: Routes = [
     canActivate: [authGuardGuard],
     loadChildren: () =>
       import('./features/dashboard/dashboard-module').then((m) => m.DashboardModule),
+  },
+  {
+    path: 'forbidden',
+    canActivate: [authGuardGuard],
+    component: Forbidden,
   },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
