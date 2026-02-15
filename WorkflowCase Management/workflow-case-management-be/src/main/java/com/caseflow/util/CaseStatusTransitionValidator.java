dacdcfbf65c9c5d54039b0238f05bd4e8545a4c5
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-02-15 23:02:55 +0530

    switch case modified

--- WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/util/CaseStatusTransitionValidator.java
+++ WorkflowCase Management/workflow-case-management-be/src/main/java/com/caseflow/util/CaseStatusTransitionValidator.java
@@ -2,16 +2,37 @@ package com.caseflow.util;
 
 import com.caseflow.domain.enums.CaseStatus;
 
+import java.util.Set;
+
 public class CaseStatusTransitionValidator {
     private CaseStatusTransitionValidator(){}
 
-    public static boolean isValid(CaseStatus from, CaseStatus to){
-        return switch (from){
-            case DRAFT -> to == CaseStatus.FILED;
-            case FILED -> to == CaseStatus.IN_REVIEW;
-            case IN_REVIEW -> to == CaseStatus.APPROVED || to == CaseStatus.REJECTED;
-            case APPROVED, REJECTED -> to == CaseStatus.CLOSED;
+    public static boolean isValid(
+            CaseStatus from,
+            CaseStatus to,
+            Set<String> roles
+    ) {
+
+        return switch (from) {
+
+            case DRAFT ->
+                    to == CaseStatus.FILED &&
+                            roles.contains("CASE_MANAGER");
+
+            case FILED ->
+                    to == CaseStatus.IN_REVIEW &&
+                            roles.contains("REVIEWER");
+
+            case IN_REVIEW ->
+                    (to == CaseStatus.APPROVED || to == CaseStatus.REJECTED) &&
+                            roles.contains("APPROVER");
+
+            case APPROVED, REJECTED ->
+                    to == CaseStatus.CLOSED &&
+                            roles.contains("ADMIN");
+
             default -> false;
         };
     }
+
 }
