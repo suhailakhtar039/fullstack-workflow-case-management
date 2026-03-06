0de0b7cd3741a99c885f2a76336012194324bc5f
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-06 22:43:20 +0530

    case detail component created

---
+++ WorkflowCase Management/workflow-case-management-fe/src/app/features/dashboard/pages/case-detail/case-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-case-detail',
+  standalone: false,
+  templateUrl: './case-detail.html',
+  styleUrl: './case-detail.css',
+})
+export class CaseDetail {
+
+}
