4c1e2355a42a686295170efd5d27d1c7ceb1fc6d
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-07 13:29:58 +0530

    case detail component modified for better readability

--- WorkflowCase Management/workflow-case-management-fe/src/app/features/dashboard/pages/case-detail/case-detail.ts
+++ WorkflowCase Management/workflow-case-management-fe/src/app/features/dashboard/pages/case-detail/case-detail.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CaseService } from '../../services/case.services';
 
 @Component({
   selector: 'app-case-detail',
@@ -6,6 +8,35 @@ import { Component } from '@angular/core';
   templateUrl: './case-detail.html',
   styleUrl: './case-detail.css',
 })
-export class CaseDetail {
+export class CaseDetail implements OnInit {
+  caseId!: number;
+  caseData: any;
+  history: any[] = [];
+  allowedTransitions: string[] = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private caseService: CaseService,
+  ) {}
+
+  ngOnInit(): void {
+    this.caseId = Number(this.route.snapshot.paramMap.get('id'));
+    this.loadCase();
+    this.loadHistory();
+    this.loadTransitions();
+  }
+
+  loadCase() {
+    this.caseService.getCaseById(this.caseId).subscribe((data) => (this.caseData = data));
+  }
+
+  loadHistory() {
+    this.caseService.getCaseHistory(this.caseId).subscribe((data) => (this.history = data));
+  }
+
+  loadTransitions() {
+    this.caseService
+      .getAllowedTransitions(this.caseId)
+      .subscribe((data) => (this.allowedTransitions = data));
+  }
 }
